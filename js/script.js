6219b4348a636b4ca6bc7b503dadb1315b438081
adnanfathino16 <fathinoadnan@gmail.com>
2023-06-09 13:50:23 +0700

    update fitur

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-// Toggle lass active
+// Toggle class active hamburger
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di click
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// toggle class active search-form
+const search = document.querySelector("#search-button");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+search.addEventListener("click", function (e) {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  // supaya saat di klik tidak scroll ke atas karena hrefnya hanya # jadi menghilangkan aksi defaultnya
+  e.preventDefault();
+});
+
 // klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
+
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
